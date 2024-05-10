0e155083cc94377dae03c949461033b06a649eb4
Christophe Thibaut <cthibauttof@gmail.com>
2024-05-10 12:16:38 +0200

    walk throug directory looking for folders

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use clap::{Parser,ValueEnum};
 use clap_num::number_range;
 use thumbnailer::{create_thumbnails, Thumbnail, ThumbnailSize};
 use std::fs::File;
-use std::io::{Write,BufReader, BufWriter};
+use std::io::BufReader;
 use std::io::Cursor;
 
 // . -> thumbnails
@@ -28,24 +28,26 @@ fn append_dir(path: &Path, dir: &str) -> PathBuf {
     dirs.join(dir).join(path.file_name().unwrap())
 }
 
-fn get_files_in_directory(dir_path: &str) -> io::Result<Vec<String>> {
+fn get_folders_in_directory(dir_path: &str) -> io::Result<Vec<String>> {
     let mut entries: Vec<String> = Vec::new();
     let source_path = Path::new(dir_path);
-    let dest_path = append_dir(source_path, "thumbnails");
     for entry in WalkDir::new(dir_path).into_iter().filter_map(|e| e.ok()) {
-        let path = entry.into_path();
-        println!("{} {}",dest_path.display(.display()), path.display());
+        if entry.file_type().is_dir() {
+            let path = entry.into_path();
+            entries.push(path.display().to_string());
+        }
     }
     Ok(entries)
 }
 fn main() {
     let args = Args::parse();
     println!("{:?}", args);
-    if let Some(path) = args.directory {
-        get_files_in_directory(path.as_str())
+    let entries = if let Some(path) = args.directory {
+        get_folders_in_directory(path.as_str())
     } else {
         panic!("can't search directory");
     };
+    println!("{:?}", entries);
     let input_file = File::open("tests/assets/test.png").unwrap();
     let reader = BufReader::new(input_file);
     let mut  thumbnails = create_thumbnails(reader, mime::IMAGE_PNG, [ThumbnailSize::Small, ThumbnailSize::Medium]).unwrap();
